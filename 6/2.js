3d10d99a72953a0ab177cd16b125e6575edde098
Avramenko Hlib <avramenko.hlib@gmail.com>
2023-12-04 20:47:31 +0200

    Signed-off-by: Avramenko Hlib <avramenko.hlib@gmail.com>

--- 6/2.js
+++ 6/2.js
@@ -1,14 +1,15 @@
 function isPalindrome(str) {
-   if (str.length === 0) {
-     return false;
-   }
-   const reversedStr = str.split('').reverse().join('');
-   
-   return str === reversedStr;
- }
- console.log(isPalindrome(''));        // false
- console.log(isPalindrome('b'));       // true
- console.log(isPalindrome('mom'));     // true
- console.log(isPalindrome('string'));  // false
- console.log(isPalindrome('bag'));     // false
- 
+
+  let len = str.length;
+  
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+console.log(isPalindrome(''));        // false
+console.log(isPalindrome('b'));       // true
